77760f1e3b80d0660608a9d6660736cb2b5b45d2
Iron <marco.jan@gmx.de>
2023-06-21 13:09:26 +0200

    init commit

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 'use strict'
 
 
-const pregame = (() => {
-    let playerNames = [];
-    let playerfigure = ['x', 'o'];
+const playerinit = (() => {
+    // Speicherung der Eingabefelder
+    const playerNames = [];
 
+    document.getElementById('start').onclick = function () {
+        const player1Name = document.getElementById("player1").value;
+        const player2Name = document.getElementById("player2").value;
+        playerNames.push(player1Name, player2Name);
+    
+        console.log(playerNames, 'Hallo');
+    };
 
+    const playerfigure = ['x','o'];
 })();
-
-
+/* 
 function playgame (){
     
-}
+} */
