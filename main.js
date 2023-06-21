fa77cec36cb70b98efde8bfaffde0760746aced5
Iron <marco.jan@gmx.de>
2023-06-21 13:31:53 +0200

    init commit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const playerinit = (() => {
         const player1Name = document.getElementById("player1").value;
         const player2Name = document.getElementById("player2").value;
         playerNames.push(player1Name, player2Name);
-    
+        
         console.log(playerNames, 'Hallo');
     };
 
